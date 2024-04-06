cf3ff12ec14fb15bc2db17ac1ec8720afb067337
Dharva-Patel <202001135@daiict.ac.in>
2024-04-06 19:49:30 +0530

    Blog Page Component added and fetched Blog data from backend

--- BlogWeb/server/server.js
+++ BlogWeb/server/server.js
@@ -294,10 +294,10 @@ app.get("/trending-blogs", (req, res) => {
 })
 
 app.post("/search-blogs", (req, res) => {
-    let { tag, query, author, page } = req.body;
+    let { tag, query, author, page, limit, eliminate_blog } = req.body;
     let findQuery;
     if(tag){
-        findQuery = { tags: tag, draft: false };
+        findQuery = { tags: tag, draft: false, blog_id: { $ne: eliminate_blog } };
     }
     else if(query){
         findQuery = { title: new RegExp(query, 'i'), draft: false };
@@ -306,7 +306,7 @@ app.post("/search-blogs", (req, res) => {
         findQuery = { author, draft:false }
     }
 
-    let maxLimit = 2;
+    let maxLimit = limit ? limit : 2;
     Blog.find(findQuery)
     .populate("author", "personal_info.profile_img personal_info.username personal_info.fullname -_id")
     .sort({ "publishedAt": -1 })
@@ -377,6 +377,26 @@ app.post("/create-blog", verifyJWT,(req, res) => {
         return res.status(500).json({ error: err.message });
     })
 
+})
+
+app.post("/get-blog", (req, res) => {
+    let { blog_id } = req.body;
+    let incrementVal = 1;
+    Blog.findOneAndUpdate({ blog_id }, { $inc : {"activity.total_reads" : incrementVal} })
+    .populate("author", "personal_info.fullname personal_info.username personal_info.profile_img")
+    .select("title des content banner activity publishedAt blog_id tags")
+    .then(blog => {
+        User.findOneAndUpdate({ "personal_info.username": blog.author.personal_info.username }, {
+            $inc : { "account_info.total_reads": incrementVal } 
+        })
+        .catch(err => {
+            return res.status(500).json({error: err.message});
+        })
+        return res.status(200).json({ blog });
+    })
+    .catch(err => {
+        return res.status(500).json({ error : err.message});
+    })
 })
 
 app.listen(port, () => {
