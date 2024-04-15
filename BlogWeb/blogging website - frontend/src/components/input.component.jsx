b323f09d3d1417052d305c1aba50bac33b8b2d5e
Dharva-Patel <202001135@daiict.ac.in>
2024-04-15 23:05:09 +0530

    Added Edit Profile Page

--- BlogWeb/blogging website - frontend/src/components/input.component.jsx
+++ BlogWeb/blogging website - frontend/src/components/input.component.jsx
@@ -1,21 +1,22 @@
 import react, { useState } from "react";
 
-function InputBox(props){
+function InputBox({ name, type, id, value, placeholder, icon, disable = false }){
     
     const [ passwordClicked, setPassword ] = useState(false);
     
     return(
         <div className="relative w-[100%] mb-4">
             <input 
-                name={props.name}
-                type={props.type === "password" ? passwordClicked ? "text" : "password" : props.type}
-                placeholder={props.placeholder}
-                defaultValue={props.value}
-                id={props.id}
+                name={name}
+                type={type === "password" ? passwordClicked ? "text" : "password" : type}
+                placeholder={placeholder}
+                defaultValue={value}
+                id={id}
+                disabled={disable}
                 className="input-box"
             />
-            <i className={"fi " + props.icon +" input-icon"}></i>
-            {props.type === "password" ?  
+            <i className={"fi " + icon +" input-icon"}></i>
+            {type === "password" ?  
                 <i className={"fi fi-rr-eye" + (passwordClicked === true ? "" : "-crossed") + " input-icon left-[auto] right-4 cursor-pointer"} onClick={() => setPassword(prevValue => prevValue = !prevValue)}></i>
             : ""} 
         </div>
